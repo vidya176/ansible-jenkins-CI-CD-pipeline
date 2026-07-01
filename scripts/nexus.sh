3ff7994dc9db3a2e1cd5fdfd1bb12020c8022b9c
vidya nandini <168907946+vidya176@users.noreply.github.com>
2026-07-01 18:58:58 +0530

    Update nexus.sh

--- scripts/nexus.sh
+++ scripts/nexus.sh
@@ -1,66 +1,28 @@
-**NEXUS
-#==================================================
 # STEP-1: CREATE EC2 INSTANCE
-#==================================================
 # OS            : Amazon Linux 2023
 # Instance Type : t2.micro (t3.micro recommended)
 # Storage       : 30 GB
 # Security Group:
 #   SSH   : 22
 #   Nexus : 8081
-
-#==================================================
 # STEP-2: UPDATE SERVER
-#==================================================
 sudo dnf update -y
-
-#==================================================
 # STEP-3: INSTALL JAVA 21, WGET & TAR
-#==================================================
 sudo dnf install java-21-amazon-corretto-devel wget tar -y
-
 # Verify Java
 java -version
-
-#==================================================
-# STEP-4: CREATE NEXUS USER
-#==================================================
-sudo useradd -r -m -d /opt/nexus -s /bin/bash nexus
-
-#==================================================
-# STEP-5: DOWNLOAD LATEST NEXUS
-#==================================================
+# STEP-4: DOWNLOAD LATEST NEXUS
 cd /opt
-
 sudo wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
-
-#==================================================
-# STEP-6: EXTRACT
-#==================================================
+# STEP-5: EXTRACT
 sudo tar -xzf latest-unix.tar.gz
-
-# Rename for convenience
-sudo mv nexus-3.* nexus
-
-#==================================================
-# STEP-7: CHANGE OWNERSHIP
-#==================================================
+# STEP-6: CHANGE OWNERSHIP
 sudo chown -R nexus:nexus /opt/nexus
-
-#==================================================
-# STEP-8: CONFIGURE NEXUS TO RUN AS nexus USER
-#==================================================
+# STEP-7: CONFIGURE NEXUS TO RUN AS nexus USER
 sudo sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
-
-#==================================================
-# STEP-9: START NEXUS
-#==================================================
+# STEP-8: START NEXUS
 sudo -u nexus /opt/nexus/bin/nexus start
-
 # Check status
 sudo -u nexus /opt/nexus/bin/nexus status
-
-#==================================================
-# STEP-10: GET INITIAL PASSWORD
-#==================================================
+# STEP-9: GET INITIAL PASSWORD
 sudo cat /opt/sonatype-work/nexus3/admin.password
